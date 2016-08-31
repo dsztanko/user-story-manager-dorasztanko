4a142cb77c374a6b922db79d16137f50c6a3fc30
dsztanko <szt.dori@gmail.com>
2016-08-31 23:01:10 +0200

    deleting function works

--- sprinter.py
+++ sprinter.py
@@ -49,29 +49,27 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-# Basic page
-@app.route("/", methods=['GET'])
-def index():
-    return redirect("/list")
 
-# List page
-@app.route("/list", methods=['GET'])
+# List and basic page
+@app.route('/', methods=['GET'])
+@app.route('/list', methods=['GET'])
 def listing():
     testing_query = query_db("""SELECT * FROM sprinter ORDER BY id ASC""")
     return render_template('list.html', query=testing_query)
 
 
 # Deleting selected user story - not working
-@app.route("/list", methods=['POST'])
+@app.route('/list/<int:story_id>', methods=['GET'])
 def deleting_user_story(story_id):
-    query_db("""DELETE FROM sprinter WHERE id={0}""".format(str(story_id)))
-    return render_template('list.html', id=story_id)
+    query_db("DELETE FROM sprinter WHERE id={0}".format(str(story_id)))
+    testing_query = query_db("""SELECT * FROM sprinter ORDER BY id ASC""")
+    return render_template('list.html', query=testing_query)
 
 
 # Story page with blank input boxes
 @app.route('/story', methods=['GET'])
 def template_test():
-    return render_template('form.html', title='Add new Story')
+    return render_template('form.html', title='Add new Story', process='creating')
 
 
 # Story page with adding new user stories
@@ -97,13 +95,13 @@ def adding_user_story():
     return redirect('/')
 
 
-# Selecting to edit
+# Selecting to edit -- not finished
 @app.route('/story/<int:story_id>', methods=['GET'])
 def selecting_for_edit(story_id):
     query = query_db("""SELECT * FROM sprinter WHERE id={0}""".format(str(story_id)))
-    return render_template('form.html', query=query, title='Edit Story', path='editing', method='post', id=story_id)
+    return render_template('form.html', query=query, title='Edit Story', method='post', id=story_id)
 
-# Editing
+# Editing -- not finished
 @app.route('/story/<int:story_id>', methods=['POST'])
 def editing(story_id):
     updated_user_story = {}
